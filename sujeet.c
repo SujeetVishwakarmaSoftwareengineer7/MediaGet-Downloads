137619250eb34a995914ac3ddd72b04e497850fb
sujeet vishwkarma <ssujeet6449@gmail.com>
2026-02-17 17:12:40 +0530

    calculator starting version

---
+++ sujeet.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    int choice;
+    printf("welcomr to simple calculator\n");
+    printf("2. simple calculator\n");
+        printf("3. scientific calculator\n");
+        printf("4. calculator close\n");
+        printf("enter the choice");
+        scanf("%d", &choice);
+        return 0;
+
+}
